95057eaf3aca218028860fa1bc9282bf13b9e3c1
Onur <onurda@code.edu.az>
2023-12-17 17:56:13 +0400

    Task

---
+++ ClassWork/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace ClassWork.Models;
+
+public class Person
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+
+    public static bool operator >(Person p1, Person p2)
+    {
+        return p1.Age > p2.Age;
+    }
+    public static int operator -(Person p1, Person p2)
+    {
+        return p1.Age-p2.Age;
+    }
+    public static int operator +(Person p1, Person p2)
+    {
+        return p1.Age+p2.Age;
+    }
+    public static bool operator <(Person p1, Person p2)
+    {
+        return (p1.Age < p2.Age);
+    }
+}
---
+++ ClassWork/Program.cs
@@ -0,0 +1,25 @@
+﻿using ClassWork.Models;
+
+Person person1 = new();
+person1.Age = 20;
+Person person2 = new();
+person2.Age = 70;
+Person person3 = new();
+person3.Age = 50;
+
+Person[] people = {person1, person2, person3};
+    for (int j = 0; j < people.Length-1; j++)
+    {
+        
+        if (people[j] > people[j + 1])
+        {
+            Person temp = people[j];
+            people[j] = people[j + 1];
+            people[j + 1] = temp;
+        }
+    }
+
+foreach (Person item in people)
+{
+    Console.WriteLine($"Age: {item.Age}");
+}
